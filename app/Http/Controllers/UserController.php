d9bb58171919d227929d588f4e63938ba396f5d4
SAsuke019 <hugo.019@hotmail.com>
2021-04-30 18:14:02 -0300

    organizando conteudo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,56 +12,43 @@ use Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index( Request $request)
     {
         $qtd_por_pagina = 5;
-        $data = User::orderBy('id','DESC')->paginate($qtd_por_pagina);
 
-        return view('users.index', compact('data'))->with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
+        $data = User::orderBy('id', 'DESC')->paginate($qtd_por_pagina);
+
+        return view('users.index', 
+                compact('data'))->
+                    with('i', ($request->input('page', 1) - 1) * $qtd_por_pagina);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
         $roles = Role::pluck('name', 'name')->all();
-        return view('userd.create', compact($roles));
+
+        return view('users.create', compact($roles));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $this->validate($request,['name' => 'required','email'=> 'required|email|unique:users,email','password' =>'required|same:confirm-password','roles' =>'required' ]);
+        $this->validate($request, ['name' => 'required','email' => 'required|email|unique:users,email',
+'password' => 'required|same:confirm-password','roles' => 'required']);
 
         $input = $request->all();
-        $input['password']= Hash::make( $input['password']);
 
-        $user =  User::create($input);
+        $input['password'] = Hash::make($input['password']);
 
-        $user->assignRole($request->input('roles'));
+        $user = User::create($input);
 
-        return redirect()->route('users.index')->with('success', 'usuario criado com sucesso');
+        $user->assignRole( $request->input('roles'));
+
+        return redirect()->route('users.index')->with('success','Usuário criado com sucesso');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
         $user = User::find($id);
@@ -69,44 +56,50 @@ class UserController extends Controller
         return view('users.show', compact('user'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         $user = User::find($id);
 
         $roles = Role::pluck('name', 'name')->all();
+
         $userRole = $user->roles->pluck('name', 'name')->all();
 
-        return view('users.edit', compact('user','roles','userRole'));
+        return view('users.edit', compact('user', 'roles', 'userRole'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,['name' => 'required','email' => 'required|email|unique:users,email','password' => 'required|same:confirm-password','roles' => 'required']);
+
+        $input = $request->all();
+
+        if(!empty($input['password'])){
+
+            $input['password'] = Hash::make($input['password']);
+
+        }else{
+
+            $input = Arr::except( $input, ['password']);
+        }
+
+        $user = User::find($id);
+
+        $user->update($input);
+
+        DB::table('model_has_roles')->where('model_id',$id)->delete();
+
+        $user->assignRole($request->input('roles'));
+
+        return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
         User::find($id)->delete();
 
-        return redirect()->route('users.index')->with('success','usuario removido com sucesso');
+        return redirect()->route('users.index')->with('success','Usuário removido com sucesso');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Htpp\Controllers\ClientesController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,9 +11,7 @@ Route::get('/', function () {
 Route::get('/avisos', function (){
     return view('avisos', ['nome' => 'hugo', 'mostrar' =>true, 'avisos' =>[['id'=>1, 'texto'=>'Feriados agora'],['id'=>2, 'texto' =>'feriados semana que vem']]]);
 });
-Route::get('/principal', function (){
-    return view ('principal', ['msg' =>'Oiii', 'aviso'=>true, 'mostrar1' => 'Olha o Cachorro', 'mostrar2'=>'Olha o Gatinho']);
-});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -31,7 +20,10 @@ Route::group(['prefix' => 'clientes'], function (){
 
 	//Controlando o acesso com o middleware auth
 	Route::get('/listar',[App\Http\Controllers\ClientesController::class, 'listar'])->middleware('auth');
-    Route::get('/recado',[App\Http\Controllers\ClientesController::class, 'recado'])->middleware('auth');
-        
 });
-   
+
+Route::group(['middleware' => ['auth']], function(){
+
+	Route::resource('/users',App\Http\Controllers\UserController::class);
+	Route::resource('/roles',App\Http\Controllers\RoleController::class);
+});
